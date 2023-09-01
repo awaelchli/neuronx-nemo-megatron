4df4e5fc36bbe1aa9a626459adc59b625a536a3c
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-09-01 11:37:38 +0000

    fix permission error

--- nemo/examples/nlp/language_modeling/gpt_6.7b.sh
+++ nemo/examples/nlp/language_modeling/gpt_6.7b.sh
@@ -9,7 +9,7 @@ export N_AH=32
 export ACT_CHKPNT_GRANULARITY=full
 
 # ./test.sh
-./test_lightning.sh
+bash ./test_lightning.sh
 
 # cd neuronx-nemo-megatron/nemo/examples/nlp/language_modeling
 # bash gpt_6.7b.sh
